7b9ca8f83a6e72f1932e928ae368776bc149dfe1
backtofayton <yucelolcay@gmail.com>
2022-03-22 17:03:05 +0200

    Fix gh react routing

--- src/index.js
+++ src/index.js
@@ -5,12 +5,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const githubBasename = `/${process.env.PUBLIC_URL}`;
-console.log('route base = ', githubBasename);
-
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename={githubBasename}>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
